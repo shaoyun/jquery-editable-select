4746fc61e2c8d3e2b2a7bfff9531b106526f1a13
shaoyun <shaoyun@yeah.net>
2016-04-25 11:18:05 +0800

    增加editbale选项控制是否启用可编辑功能

--- jquery-editable-select.js
+++ jquery-editable-select.js
@@ -10,28 +10,33 @@
 
     $.fn.editableSelect = function(options) {
         var defaults = {
-            warpClass: 'ui-select-wrap'
+            warpClass: 'ui-select-wrap',
+            editable: true
         };
         var opts = $.extend(defaults,options);
 
         $(this).each(function(){
-
+            var $select     = $(this);
+            if($select.attr('data-editable') && $select.attr('data-editable') == 'false') {
+                opts.editable = false;
+            }
             var html = '<div class="' + opts.warpClass + '" tabindex="-1">'
             + '<div class="selected"><span></span>'
             + '<a class="ui-button"><i class="fa fa-angle-down"></i></a></div>'
-            + '<div class="dropdown-box"><div class="inputbox"><input type="text" placeholder="Enter your text" /></div>'
-            + '<ul></ul></div></div>';
-            var $select     = $(this);
+            + '<div class="dropdown-box">';
+            html += (opts.editable ?
+                '<div class="inputbox"><input type="text" placeholder="Enter your text" /></div>' : '');
+
+            html += '<ul></ul></div></div>';
+
             var $selectWrap = $(html).insertAfter(this);
             var $options    = $selectWrap.find('ul');
             var $downbox = $selectWrap.find(".dropdown-box");
-            var $input = $downbox.find("input");
             var bEdit = false;
             $select.hide();
             $downbox.css({
                 'width' : $selectWrap.width() + 'px'
             });
-            $downbox.find(".inputbox").css('width', $selectWrap.width() - 22 + 'px');
             var _opt_click = function() {
                 var val  = $(this).attr('data-val'), txt = $(this).text();
                 $selectWrap.find(".selected").attr("data-val",val).find('span').text(txt);
@@ -76,24 +81,28 @@
                  setTimeout(function() { if(bEdit) return; _show(false);}, 100);
                  $selectWrap.removeClass('focus');
             });
-            $input.focus(function(){
-                  $selectWrap.addClass('focus');
-                  bEdit = true;
-            });
-            $input.blur(function() {
-                bEdit = false;
-                $selectWrap.trigger('blur');
-            });
-            $input.bind('keypress',function(event){
-                if(event.keyCode == "13"){
-                    var val = $input.val();
-                    var $opt = _new_opt(val, val, true);
-                    $opt.appendTo($options);
-                    $input.val('');
-                    $("<option>").val(val).text(val).appendTo($select);
-                    $opt.trigger('click');
-                }
-            });
+            if(opts.editable) {
+                var $input = $downbox.find("input");
+                $downbox.find(".inputbox").css('width', $selectWrap.width() - 22 + 'px');
+                $input.focus(function(){
+                      $selectWrap.addClass('focus');
+                      bEdit = true;
+                });
+                $input.blur(function() {
+                    bEdit = false;
+                    $selectWrap.trigger('blur');
+                });
+                $input.bind('keypress',function(event){
+                    if(event.keyCode == "13"){
+                        var val = $input.val();
+                        var $opt = _new_opt(val, val, true);
+                        $opt.appendTo($options);
+                        $input.val('');
+                        $("<option>").val(val).text(val).attr('data-is-input', 1).appendTo($select);
+                        $opt.trigger('click');
+                    }
+                });
+            }
         });
     }
 })(jQuery);
